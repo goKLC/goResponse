def0942bd513fc7d86b2c2ed5cd15794aeeec472
Mustafa Kılıç <mustafa.kilic@beyn.com.tr>
2021-07-04 16:31:11 +0300

    go response

--- response.go
+++ response.go
@@ -1,37 +1,53 @@
 package goResponse
 
 type Response struct {
-
+	headers    map[string]string
+	body       string
+	statusCode int
 }
 
-func (r *Response) GetHeaders() map[string]string  {
+func NewGoResponse() *Response {
+	return &Response{
+		headers: map[string]string{},
+	}
+}
 
+func (r *Response) GetHeaders() map[string]string {
+	return r.headers
 }
 
 func (r *Response) HasHeaders(name string) bool {
+	_, ok := r.headers[name]
 
+	return ok
 }
 
-func (r *Response) GetHeader(name string) string  {
+func (r *Response) GetHeader(name string) string {
+	header, ok := r.headers[name]
 
+	if !ok {
+		header = ""
+	}
+
+	return header
 }
 
 func (r *Response) WithHeader(name string, value string) {
-
+	r.headers[name] = value
 }
 
-func (r *Response) WithBody(value string)  {
-
+func (r *Response) WithBody(value string) {
+	r.body = value
 }
 
 func (r *Response) GetBody() string {
-
+	return r.body
 }
 
 func (r *Response) GetStatusCode() int {
-
+	return r.statusCode
 }
 
 func (r *Response) WithStatusCode(code int) {
-
+	r.statusCode = code
 }
